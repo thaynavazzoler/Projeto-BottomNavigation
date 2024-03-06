1b3b6a18879d487abdc392612c736d4d2bc27b4b
thaynavazzoler <thayna.vazzoler@aluno.senai.br>
2024-03-06 08:54:47 -0300

    "Create contact and pages"

---
+++ src/screens/Contact/index.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import styles from "./style.js";
+
+export default function Contact() {
+  return (
+    <View style={styles.container}>
+      <Text>Contact</Text>
+    </View>
+  );
+}
---
+++ src/screens/Profile/index.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import styles from "./style.js";
+
+export default function Profile() {
+  return (
+    <View style={styles.container}>
+      <Text>Profile</Text>
+    </View>
+  );
+}
