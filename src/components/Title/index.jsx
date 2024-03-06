2236fc90e236291fdf614ab1de36faa17e6e0452
thaynavazzoler <thayna.vazzoler@aluno.senai.br>
2024-03-06 08:19:06 -0300

    "refactor: project structor"

---
+++ src/components/Title/index.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React from "react";
+import styles from "./style.js";
+
+const Title = () => {
+  return (
+    <View>
+      <Text style={styles.title}>title</Text>
+    </View>
+  );
+};
+
+export default Title;
---
+++ src/screens/Home/index.jsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+import styles from "./style.js";
+
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Text>Home</Text>
+    </View>
+  );
+}
