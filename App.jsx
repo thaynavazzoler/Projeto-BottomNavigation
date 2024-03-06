3363bc031cd1fa25089916f0ad15354f008250a5
thaynavazzoler <thayna.vazzoler@aluno.senai.br>
2024-03-06 09:49:38 -0300

    "app.jsx"

--- App.jsx
+++ App.jsx
@@ -1,8 +1,11 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { StatusBar } from "expo-status-bar";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
 import Home from "./src/screens/Home";
+import Contact from "./src/screens/Contact";
+import Profile from "./src/screens/Profile";
 
 const Tab = createBottomTabNavigator();
 
@@ -10,7 +13,39 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            tabBarLabel: "Início",
+            tabBarIcon: ({}) => (
+              <MaterialCommunityIcons name="home" color={"blue"} size={32} />
+            ),
+          }}
+        />
+
+        <Tab.Screen
+          name="Contact"
+          component={Contact}
+          options={{
+            tabBarLabel: "Contato",
+            tabBarIcon: ({}) => (
+              <MaterialCommunityIcons name="phone" color={"blue"} size={32} />
+            ),
+            tabBarBadge: "saudades de vocês!",
+          }}
+        />
+
+        <Tab.Screen
+          name="Profile"
+          component={Profile}
+          options={{
+            tabBarLabel: "Perfil",
+            tabBarIcon: ({}) => (
+              <MaterialCommunityIcons name="account" color={"blue"} size={32} />
+            ),
+          }}
+        />
       </Tab.Navigator>
 
       <StatusBar style="auto" />
